677404cc9ade5fbabd16ed532a044adcbea78fee
Josue Pereira Leiva <b04751@ecci.ucr.ac.cr>
2016-09-27 10:07:41 -0600

    Initial Commit Base Project

---
+++ ProyectoInge1/ProyectoInge1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoInge1.MvcApplication" Language="C#" %>
---
+++ ProyectoInge1/ProyectoInge1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoInge1.Startup))]
+namespace ProyectoInge1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
