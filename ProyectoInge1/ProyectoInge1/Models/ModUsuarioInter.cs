6e1dfe3907d985eb1969e180336619cf0859c728
Josue Pereira Leiva <b04751@ecci.ucr.ac.cr>
2016-09-30 12:49:11 -0600

    creación EF, parcial Usuarios

---
+++ ProyectoInge1/ProyectoInge1/Models/ModUsuarioInter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoInge1.Models
+{
+    public class ModUsuarioInter
+    {
+        public Usuario modeloUsuario { get; set; }
+        public Telefono modeloTelefono { get; set; }
+        public Proyecto modeloProyecto { get; set; }
+        public List<Usuario> listaUsuarios { get; set; }
+        public List<Telefono> listaTelefono { get; set; }
+        public List<Proyecto> listaProyectos { get; set; }
+     }
+}
