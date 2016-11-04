76c2cf0e2fece31632e37849f865d7306163ce55
Jose Chaves Jimenez <b11882@ecci.ucr.ac.cr>
2016-11-03 19:14:12 -0600

    criterio de aceptacion agregado, modificar agregado pero no terminado

--- ProyectoInge1/ProyectoInge1/Controllers/ProyectosController.cs
+++ ProyectoInge1/ProyectoInge1/Controllers/ProyectosController.cs
@@ -122,7 +122,7 @@ namespace ProyectoInge1.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Detalles(ModProyectoInter modelo)
         {
-
+            BD.Entry(modelo.proyecto).State = EntityState.Modified;
             return RedirectToAction("Index");
         }
 
@@ -137,55 +137,10 @@ namespace ProyectoInge1.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(ModUsuarioInter modelo, string id)
+        public async Task<ActionResult> Create(ModUsuarioInter modelo)
         {
-            /*if (ModelState.IsValid)
-            {
-                
-                var UserManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                var RoleManager = Request.GetOwinContext().Get<ApplicationRoleManager>();
-                var user = new ApplicationUser { UserName = modelo.modeloUsuario.correo, Email = modelo.modeloUsuario.correo };
-                var result = await UserManager.CreateAsync(user, password);
-                */
-            /*if (result.Succeeded)
-            {
-                modelo.modeloUsuario.id = user.Id;
-
-                BD.Usuario.Add(modelo.modeloUsuario);
-                BD.SaveChanges();
-
-                if (modelo.modeloTelefono1.numero != null)
-                {
-                    modelo.modeloTelefono1.usuario = modelo.modeloUsuario.cedula;
-                    BD.Telefono.Add(modelo.modeloTelefono1);
-                }
-                if (modelo.modeloTelefono2.numero != null)
-                {
-                    modelo.modeloTelefono2.usuario = modelo.modeloUsuario.cedula;
-                    BD.Telefono.Add(modelo.modeloTelefono2);
-                }
-                BD.SaveChanges();
-
-                var roleId = modelo.Role;
-                var role = await RoleManager.FindByIdAsync(roleId);
-                var result2 = await UserManager.AddToRoleAsync(modelo.modeloUsuario.id, role.Name);
-
-                if (result2.Succeeded)
-                {
-                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(modelo.modeloUsuario.id);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = modelo.modeloUsuario.id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(modelo.modeloUsuario.id, "Ingreso al sistema", "Su contraseña temporal asignada es " + password + "\n" + "Por favor confirme su cuenta pulsando click <a href=\"" + callbackUrl + "\">aquí</a>");
-
-                }
-
-            }
-            return RedirectToAction("Index");
-        }
-        else
-        {
-            ModelState.AddModelError("", "Debe completar toda la información necesaria.");*/
+            
             return View(modelo);
-            //}
         }
 
         [HttpPost]
