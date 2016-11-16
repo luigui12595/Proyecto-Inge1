c33377ad1cef946e5db32af7488c3b27fe4c101a
kfonseca <kfonseca.hp@gmail.com>
2016-11-16 09:48:45 -0600

    cambios en vista de modificar requerimiento funcional y creacion de modelo y controladora de modulo gestion cambios

---
+++ ProyectoInge1/ProyectoInge1/Models/ModGestionCambios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoInge1.Models
+{
+    public class ModGestionCambios
+    {
+        public List<HistVersiones> listaCambios { get; set;  }
+        public List<Proyecto> listaProyectos { get; set; }
+
+    }
+}
